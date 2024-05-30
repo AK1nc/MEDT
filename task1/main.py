9c9669f9f6d1904c3c9d58e7db8bce0a83779e2b
AK1nc <125077642+AK1nc@users.noreply.github.com>
2024-05-31 01:32:27 +0300

    main.py

--- task1/main.py
+++ task1/main.py
@@ -1,5 +1,6 @@
 import math
 from xml.dom import minidom
+from matplotlib import pyplot as plt
 import numpy
 
 
@@ -7,7 +8,9 @@ LEFT_BOLDER = -15.
 RIGHT_BOLDER = 15.
 STEP = 0.1
 CONST_A = 1.34941
-
+# создаем массивы для заполнения точками
+x_t = []
+y_t = []
 def f_x(x):
     return -0.0001 * (math.fabs(math.sin(x) * math.sin(CONST_A) * math.exp(math.fabs(100 - (math.sqrt(x ** 2 + CONST_A ** 2) / math.pi)))) + 1) ** 0.1
 
@@ -24,9 +27,14 @@ for x in numpy.arange(LEFT_BOLDER, RIGHT_BOLDER, STEP):
     productChild = root.createElement('row')
     productChild.appendChild(tempRowX)
     productChild.appendChild(tempRowY)
-
+    
+    # что бы два раза не считать f_x создадим временную переменную
+    tempF_X = f_x(x)
     sizeX = root.createTextNode(str(x))
-    sizeY = root.createTextNode(str(f_x(x)))
+
+    x_t.append(float(x))
+    y_t.append(float(tempF_X))
+    sizeY = root.createTextNode(str(tempF_X))
 
     tempRowX.appendChild(sizeX)
     tempRowY.appendChild(sizeY)
@@ -35,6 +43,14 @@ for x in numpy.arange(LEFT_BOLDER, RIGHT_BOLDER, STEP):
 
 xml_str = root.toprettyxml(indent="\t")
 
+# используя matplotlib создаем график
+plt.plot(x_t, y_t)
+# Adding title, xlabel and ylabel
+plt.title('')
+plt.xlabel('X-axis')
+plt.ylabel('Y-axis')
+plt.show()
 save_path_file = "results.xml"
+
 with open(save_path_file, "w") as f:
     f.write(xml_str)
